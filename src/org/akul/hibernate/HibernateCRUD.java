919352a7c4c0f3efcd50c9f1ff3d8875017ac93f
akul <akul.sharma007@gmail.com>
2017-03-07 22:59:33 +0530

    updated with basics of HQL

--- src/org/akul/hibernate/HibernateCRUD.java
+++ src/org/akul/hibernate/HibernateCRUD.java
@@ -1,6 +1,9 @@
 package org.akul.hibernate;
 
+import java.util.List;
+
 import org.akul.dto.UserDetails;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -8,8 +11,7 @@ import org.hibernate.cfg.Configuration;
 public class HibernateCRUD {
 
 	public static void main(String[] ar){
-		UserDetails user = new UserDetails();
-		user.setUserName("user" );
+		
 		
 		
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
@@ -17,14 +19,14 @@ public class HibernateCRUD {
 		Session session = sessionFactory.openSession();
 		//beginning the transaction,i can save as many transactions as i want and it defines the single unit of work
 		session.beginTransaction();
-		/*
+		
 		//below is creating
 		for(int i=0;i<10;i++){
 			UserDetails usermul =new UserDetails();
 			usermul.setUserName("user "+i);
 			usermul.setDescription("user "+i+"description");
 			session.save(usermul);
-		}*/
+		}
 		/*
 		//fetching of data after creating
 		UserDetails user = (UserDetails)session.get(UserDetails.class, 6);//second argument is the primary key value for which we need to fetch data
@@ -35,13 +37,24 @@ public class HibernateCRUD {
 		user.setUserName("updated user");
 		session.update(user);*/
 		//ending the transaction
-		session.save(user);
 		
-		user.setUserName("updated");//IMPORTANT-even if we din ask hibernate to save the updated value again,hibernate did it on its own and that is why it is calld to trace the object and updation happens on its own
+		
+		//user.setUserName("updated");//IMPORTANT-even if we din ask hibernate to save the updated value again,hibernate did it on its own and that is why it is calld to trace the object and updation happens on its own
+		
+		//HQL
+		//create query object
+		Query query = session.createQuery("select userName from UserDetails");
+		query.setFirstResult(5);//pagination start from 5th record
+		query.setMaxResults(2);//and display only 2 records
+		
+		
+		//List<UserDetails> users = (List<UserDetails>) query.list();
+		List<String> usernames = (List<String>) query.list();
 		session.getTransaction().commit();
 		
 		session.close();
-		
+		for(String u : usernames)
+		System.out.println(u);
 		
 	}
 }
